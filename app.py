0d26a15bdeeaf42496c44ba8cf886fa808b67f78
Lucas Souza <lucashenriquedesouzall@gmail.com>
2021-08-13 15:54:25 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
+
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)                                                                                                                           
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Laboratório Pipeline DevOps"
 
 @app.route('/bug')                                                                                                                                
 def bad():                                                                                                                                        
     try:                                                                                                                                          
         raise TypeError()                                                                                                                         
-     except TypeError as e:                                                                                                                        
+    except TypeError as e:                                                                                                                        
         print(e)                                                                                                                                  
-     except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
